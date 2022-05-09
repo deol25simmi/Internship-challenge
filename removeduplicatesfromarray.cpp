f0eac587a5886731d279539fdc3fd1238d59fa8f
deol25simmi <105154354+deol25simmi@users.noreply.github.com>
2022-05-09 20:05:00 +0530

    Add files via upload

---
+++ removeduplicatesfromarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+          nums[count] = nums[i];
+                count++;
+        }
+      
+    }
+         nums[count++] = nums[nums.size()-1];
+            return count;
+    }
+};
